46fa2f8af2013e7a675b1c8c3ddc6875314a5115
Alex <god18007@byui.edu>
2020-05-05 22:06:15 -0600

    changed php and html

--- 03Teach/vars.php
+++ 03Teach/vars.php
@@ -1,9 +1,9 @@
 <?php
-$username = htmlspecialchars($_POST['username']);
-$email = htmlspecialchars($_POST['emailAddress']);
-$major = htmlspecialchars($_POST['major']);
-$comments = htmlspecialchars($_POST['comments']);
-$continents = ($_POST['Continents']);
+//$username = htmlspecialchars($_POST['username']);
+//$email = htmlspecialchars($_POST['emailAddress']);
+//$major = htmlspecialchars($_POST['major']);
+//$comments = htmlspecialchars($_POST['comments']);
+//$continents = ($_POST['Continents']);
 ?>
 
 <!DOCTYPE html>
@@ -13,18 +13,18 @@ $continents = ($_POST['Continents']);
 
 <body>
 <h1> Welcome user: <?php echo $username; ?></h1>
-<h1> mailto: <a href="mailto:<?=$email ?>"><?=$email ?></a></h1>
-<h1> Major: <?php echo $major; ?></h1>
+<!--<h1> mailto: <a href="mailto:<?=$email ?>"><?=$email ?></a></h1>
+<h1> Major: <?php echo $major; ?></h1>-->
 <h1> Continents Visited:</h1>
 <ul>
 <?
-foreach ($continents as $continents){
+/*foreach ($continents as $continents){
     $_continents=htmlspecialchars($continents);
     echo "<li><p>$_continents</p></li>";
-}
+}*/
 ?>
 </ul>
-<h1> Comments: <?php echo $comments; ?></h1>
+<!--<h1> Comments: <?php echo $comments; ?></h1>-->
 
 </body>
 
