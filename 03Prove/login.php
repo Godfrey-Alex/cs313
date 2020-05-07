71827f5029e38d442eb1b8c9554deca5bd0009e9
Alex <god18007@byui.edu>
2020-05-06 23:20:37 -0600

    changing stuff

--- 03Prove/login.php
+++ 03Prove/login.php
@@ -59,45 +59,22 @@ else if(array_key_exists('saber', $_POST)) {
 <form method="post">
 <img src="images/Artoo.PNG" alt="Artoo" height=25% width=auto><br>
   <p>Remote Control R2-D2<br>$29.99</p>
-  <input type="submit" class="button" name="artoo" value="Add to Cart" /><br>
+  <input type="submit" class="button" name="artoo" value="Add to Cart" /><br><br>
 
   <img src="images/ATST.PNG" alt="AT-ST" height=25% width=auto><br>
   <p>Model AT-ST<br>$45.99</p>
-  <input type="submit" class="button" name="atst" value="Add to Cart"/><br>
+  <input type="submit" class="button" name="atst" value="Add to Cart"/><br><br>
 
   <img src="images/DarthVader.PNG" alt="Darth Vader" height=25% width=auto><br>
   <p>Darth Vader Mask<br>$59.99</p>
-  <input type="submit" class="button" name="vader" value="Add to Cart"/><br>
+  <input type="submit" class="button" name="vader" value="Add to Cart"/><br><br>
 
   <img src="images/lightSaber.PNG" alt="Light Saber" height=25% width=auto><br>
   <p>LightSaber Collection Set<br>$129.99</p>
-  <input type="submit" class="button" name="saber" value="Add to Cart"/><br>
+  <input type="submit" class="button" name="saber" value="Add to Cart"/><br><br>
   </form>
 <br>
 <br>
-
-<form action="login.php" method="post" id="frmLogin">
-  <div><label for="login">Username</label></div>
-  <div><input name="user_name" type="text"></div>
-  <br>
-  <img src="images/Artoo.PNG" alt="Artoo" height=25% width=auto><br>
-  <p>Remote Control R2-D2<br>$29.99</p>
-  <input type="checkbox" name="item[]" value="R2-D2"> Add to cart<br>
-  <img src="images/ATST.PNG" alt="AT-ST" height=25% width=auto><br>
-  <p>Model AT-ST<br>$45.99</p>
-  <input type="checkbox" name="item[]" value="AT-ST"> Add to cart<br>
-  <img src="images/DarthVader.PNG" alt="Darth Vader" height=25% width=auto><br>
-  <p>Darth Vader Mask<br>$59.99</p>
-  <input type="checkbox" name="item[]" value="DarthVader Helmet"> Add to cart<br>
-  <img src="images/lightSaber.PNG" alt="Light Saber" height=25% width=auto><br>
-  <p>LightSaber Collection Set<br>$129.99</p>
-  <input type="checkbox" name="item[]" value="Light Saber"> Add to cart<br>
-
-
-
-
-  <div><input type="submit" name="login" value="go to cart"></span></div>  
-</form>
 
 <script src="cart.js"></script>
 </body>
