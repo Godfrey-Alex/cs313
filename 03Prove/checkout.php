ed542e27c6b3a456e39844b5160978ea71812271
Alex <god18007@byui.edu>
2020-05-08 01:00:45 -0600

    changed stuff

--- 03Prove/checkout.php
+++ 03Prove/checkout.php
@@ -14,13 +14,13 @@ print_r($_SESSION);
 <h1>Checkout</h1>
 
 <form method="post">
-<button type="submit" formaction="/03Prove/cart.php">Return to Cart</button>
+<button type="submit" formaction="/03Prove/cart.php">Return to Cart</button><br>
 <!--<button type="submit" formaction="/03Prove/confirm.php">Buy Now</button>-->
 </form>
 
 <form action="confirm.php" method="post">
 Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+E-mail: <input type="text" name="email"><br><br>
 <input type="submit">
 </form>
 
