2bbf9a2242dc674dd89f34ad532aee14d3d982c6
Alex <god18007@byui.edu>
2020-05-08 22:34:02 -0600

    chaged stuff

--- 03Prove/confirm.php
+++ 03Prove/confirm.php
@@ -12,14 +12,27 @@ print_r($_SESSION);
 echo $_POST["name"];
 $_SESSION["name"]=$_POST["name"];
 echo $_SESSION["name"];
-//$_SESSION["email"]=$_POST["email"];
-//$_SESSION["address"]=$_POST["address"];
-//$_SESSION["city"]=$_POST["city"];
-//$_SESSION["zip"]=$_POST["zip"];
-//$_SESSION["state"]=$_POST["state"];
+$_SESSION["email"]=$_POST["email"];
+$_SESSION["address"]=$_POST["address"];
+$_SESSION["city"]=$_POST["city"];
+$_SESSION["zip"]=$_POST["zip"];
+$_SESSION["state"]=$_POST["state"];
+print_r($_SESSION);
 ?>
 
 <h1>Thank you for your Purchase</h1>
+<h2>
+<?php
+echo "Your order will shipped in 3-5 business days to the following
+    information...<br>"
+    . $_SESSION["name"] . "<br>"
+    . $_SESSION["address"] . "<br>"
+    . $_SESSION["city+"] . "<br>"
+    . $_SESSION["zip"] . "<br>"
+    . $_SESSION["state"] . "<br><br>You should have recieved an invoice at " . $_SESSION["email"] . "";
+
+?>
+</h2>
 
 <form method="post">
 <button type="submit" formaction="/03Prove/cart.php">Return to Cart</button>
