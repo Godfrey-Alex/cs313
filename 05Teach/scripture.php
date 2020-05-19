2dffe03aaf3e08469bdcd2c6456601d9f0cbd57e
Alex <god18007@byui.edu>
2020-05-18 22:59:36 -0600

    adding code to connect to heroku db

--- 05Teach/scripture.php
+++ 05Teach/scripture.php
@@ -9,6 +9,28 @@
 <?php
 session_start();
 //print_r($_SESSION);
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
 ?>
 
 <h1>Behold my Scriptures:</h1>
