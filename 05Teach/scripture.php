f61c41484735b8f41a3f536c2e3170ab324cd7d0
Alex <god18007@byui.edu>
2020-05-18 22:44:47 -0600

    Added Scriptures repository

---
+++ 05Teach/scripture.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "en">   
+<head>
+<title>Scriptures</title>
+<link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+
+<?php
+session_start();
+//print_r($_SESSION);
+?>
+
+<h1>Behold my Scriptures:</h1>
+
+
+
+
+<script src="cart.js"></script>
+</body>
+</html>
