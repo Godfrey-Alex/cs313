a3ce438dbf3ea3944ac5d5711cd998e8fdbd20ce
Alex <god18007@byui.edu>
2020-05-21 00:01:40 -0600

    added project folder

--- Project/home.php
+++ Project/home.php
@@ -54,7 +54,7 @@ catch (PDOException $ex)
 <h1>Behold your friends:</h1>
 
 <?php
-$db->query('SELECT id FROM public.user WHERE username = $username AND password = $password');
+//$db->query('SELECT id FROM public.user WHERE username = $username AND password = $password');
 foreach ($db->query('SELECT id FROM public.user WHERE username = $username AND password = $password') as $row){
     echo '' . $row['id'];
 
