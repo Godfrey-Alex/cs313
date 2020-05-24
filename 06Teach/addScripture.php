900f092a484478139b7649b5ad7abb6d4f926cf5
Alex <god18007@byui.edu>
2020-05-24 15:37:21 -0600

    adding scripture update form

---
+++ 06Teach/addScripture.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang = "en">   
+<head>
+<title>Scriptures</title>
+<link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+
+<?php
+session_start();
+//print_r($_SESSION);
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+
+<h1>Add a Scriptures</h1>
+
+<html>
+<body>
+
+<form action="/06Teach/scripture.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
+
+
+<script src="cart.js"></script>
+</body>
+</html>
