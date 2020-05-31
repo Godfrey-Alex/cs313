2584896f8df6439b94c968ad34cc6719e41d5106
Alex <god18007@byui.edu>
2020-05-30 23:22:31 -0600

    added ability to add memory to a friend

--- Project/addMemory.php
+++ Project/addMemory.php
@@ -49,18 +49,18 @@ if(array_key_exists('addMemory', $_POST)) {
   $query = 'INSERT INTO public.memory(memory_name, memory_date, memory_text) VALUES(:memory_name, :memory_date, :memory_text)';
   $statement = $db->prepare($query);
   $statement->bindValue(':memory_name', $mTitle);
-  //$statement->bindValue(':memory_date', $mDate;
-  //$statement->bindValue(':memory_text', $mText;
-  //$statement->execute();
-  //$lastMemoryId = $db->lastInsertId("memory_id_seq");
+  $statement->bindValue(':memory_date', $mDate);
+  $statement->bindValue(':memory_text', $mText);
+  $statement->execute();
+  $lastMemoryId = $db->lastInsertId("memory_id_seq");
 
-  //$query1 = 'INSERT INTO public.memory_list (user_id, friend_id, memory_id) VALUES (:user_id,:friend_id, :memory_id)';
-  //echo $query1;
-  //$statement = $db->prepare($query1);
-  //$statement->bindValue(':user_id', $_SESSION["currentUserId"];
-  //$statement->bindValue(':friend_id', $_SESSION["viewFriendId"];
-  //$statement->bindValue(':memory_id', $lastMemoryId;
-  //$statement->execute();
+  $query1 = 'INSERT INTO public.memory_list (user_id, friend_id, memory_id) VALUES (:user_id,:friend_id, :memory_id)';
+  echo $query1;
+  $statement = $db->prepare($query1);
+  $statement->bindValue(':user_id', $_SESSION["currentUserId"]);
+  $statement->bindValue(':friend_id', $_SESSION["viewFriendId"]);
+  $statement->bindValue(':memory_id', $lastMemoryId);
+  $statement->execute();
 
   header("Location: https://young-hollows-53465.herokuapp.com/Project/home.php");
   } 
