75817e6bf5396a27f82f5bd18902a03de65a516e
Alex <god18007@byui.edu>
2020-05-30 22:15:29 -0600

    added ability to add memory to a friend

--- Project/addMemory.php
+++ Project/addMemory.php
@@ -40,7 +40,8 @@ catch (PDOException $ex)
 }
 
 if(array_key_exists('addMemory', $_POST)) { 
-  //echo $_POST["mTitle"]);
+  echo 'it worked';
+    //echo $_POST["mTitle"]);
   //$query = 'INSERT INTO friend(display_name) VALUES(:display_name)';
   //$statement = $db->prepare($query);
   //$statement->bindValue(':display_name', $_POST["nfDisplay_name"]);
