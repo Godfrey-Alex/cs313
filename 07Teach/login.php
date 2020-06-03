50aac2f74ed642fdc2ee657e7e70690e0e8e902e
Alex <god18007@byui.edu>
2020-06-02 22:03:47 -0600

    adding login page assignment

---
+++ 07Teach/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang = "en">   
+<head>
+<title>Login</title>
+<link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+
+<?php
+session_start();
+
+//print_r($_SESSION);
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+if(array_key_exists('loginPost', $_POST)) { 
+  echo 'you hit login';
+} 
+
+?>
+
+<h1>Please Log In</h1>
+
+
+
+<form method="post">
+username: <input type="text" name="username"><br>
+password: <input type="password" name="password"><br>
+<input type="submit" value="Login" name="loginPost">
+</form>
+
+
+
+<script src="cart.js"></script>
+</body>
+</html>
