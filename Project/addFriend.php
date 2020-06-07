a308d863883419d45bf3d6ebdd23ffeb58850b9e
Alex <god18007@byui.edu>
2020-06-06 23:13:25 -0600

    adding login page

--- Project/addFriend.php
+++ Project/addFriend.php
@@ -9,9 +9,7 @@
 <?php
 session_start();
 echo $_SESSION["currentUserId"];
-
 //print_r($_SESSION);
-
 try
 {
   $dbUrl = getenv('DATABASE_URL');
@@ -34,15 +32,42 @@ catch (PDOException $ex)
   die();
 }
 print_r($_SESSION);
+
+if(array_key_exists('addFriend', $_POST)) { 
+  echo $_POST['nfDisplay_name'];
+  $nFriend = $_POST['nfDisplay_name'];
+  $query = 'INSERT INTO public.friend(display_name) VALUES(:display_name)';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':display_name', $nFriend);
+  $statement->execute();
+  $lastFriendId = $db->lastInsertId("friend_id_seq");
+
+
+  echo 'last friend id: '.$lastFriendId;
+  $query1 = 'INSERT INTO public.user_friend_list (user_id, friend_id) VALUES (:user_id, :friend_id)';
+  echo $query1;
+  $statement = $db->prepare($query1);
+  $statement->bindValue(':user_id', $_SESSION["currentUserID"]);
+  $statement->bindValue(':friend_id', $lastFriendId);
+  $statement->execute();
+
+  header("Location: https://young-hollows-53465.herokuapp.com/Project/home.php");
+  } 
 ?>
 
 <h1>Please give the name of your new friend.</h1>
 
 
 
-<form action="/Project/home.php" method="post">
+<!--<form action="/Project/home.php" method="post">
 New Friend Name: <input type="text" name="nfDisplay_name"><br>
 <input type="submit" value="Add Friend" name="addNewFriend">
+</form>
+-->
+
+<form method="post">  
+New Friend Name: <input type="text" name="nfDisplay_name"><br>  
+<input type="submit" class="button" name="addFriend" value="Add Memory" /><br><br>
 </form>
 
 
