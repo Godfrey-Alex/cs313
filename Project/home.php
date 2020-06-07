849f37034eaf47cac0234ca588926b188f1d67b7
Alex <god18007@byui.edu>
2020-06-06 23:44:39 -0600

    adding login page

--- Project/home.php
+++ Project/home.php
@@ -31,6 +31,10 @@ catch (PDOException $ex)
   die();
 }
 
+if(!isset($_SESSION['currentUserID'])){
+  header("Location: https://young-hollows-53465.herokuapp.com/Project/login.php");
+}
+
 
 if(array_key_exists('logout', $_POST)) {
   unset($_SESSION["currentUserID"]);
